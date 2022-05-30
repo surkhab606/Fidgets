c5b3d68db7fd28f610314bc5017a085b7f8b9319
905041004 <905041004@L815WD220029.edu.cbe.ab.ca>
2022-05-30 11:50:50 -0600

    DONE

---
+++ GettingStarted/src/GettingStarted.java
@@ -0,0 +1,56 @@
+
+//Add Phidgets Library | You added a file called phidget22 when configuring your project. Import gives you access to the Phidgets library code inside that file. 
+import com.phidget22.*;
+
+public class GettingStarted {	
+    //Handle Exceptions | Exceptions will happen in your code from time to time. These are caused by unexpected things happening. Make sure you’ve added "throws Exception" to your main method.
+    public static void main(String[] args) throws Exception{
+
+        //Create | Create objects for your buttons and LEDs.
+        DigitalInput redButton = new DigitalInput();
+        DigitalOutput redLED = new DigitalOutput();
+        DigitalInput greenButton = new DigitalInput();
+        DigitalOutput greenLED = new DigitalOutput();
+
+        //Address | Address your four objects which lets your program know where to find them.
+        redButton.setHubPort(0);
+        redButton.setIsHubPortDevice(true);
+        redLED.setHubPort(1);
+        redLED.setIsHubPortDevice(true);
+        greenButton.setHubPort(5);
+        greenButton.setIsHubPortDevice(true);
+        greenLED.setHubPort(4);
+        greenLED.setIsHubPortDevice(true);
+
+        //Open | Connect your program to your physical devices.
+        redButton.open(1000);
+        redLED.open(1000);
+        greenButton.open(1000);
+        greenLED.open(1000);
+
+        //Use your Phidgets | This code will turn on the LED when the matching button is pressed and turns off the LED when the matching button is released. The sleep function slows down the loop so the button state is only checked every 150ms.
+        while(true){
+        	
+        	//Modified code so that LED remains on until the button is pressed. When the button is pressed the LED will turn off. 
+        	//Modified code so that the greenButton code controls the redLED and vice versa.
+            if(greenButton.getState())
+            {
+                redLED.setState(false);
+            } else 
+            	
+            {
+                redLED.setState(true);
+            }
+
+            if(redButton.getState()){
+                greenLED.setState(false);
+            } else {
+                greenLED.setState(true);
+            }
+
+            Thread.sleep(150);
+        }
+    }
+}
+  
+  
